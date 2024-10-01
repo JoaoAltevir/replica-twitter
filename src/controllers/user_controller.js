d7427102c145cdfeef55f545780454bf108ecaaf
João Neto <j.altevirneto@gmail.com>
2024-10-01 11:07:46 -0300

    feat: add logic for users

---
+++ src/controllers/user_controller.js
@@ -0,0 +1,37 @@
+import User from "../models/user_model.js";
+
+
+const signup = async (req,res) => {
+    try {
+        const user = await User.create({
+            email: req.body.email,
+            password: req.body.password,
+            nickname: req.body.nickname,
+        })
+
+        const token = jwtServices.generateAccessToken(user)
+
+        res.status(201).json(token)
+    } catch (err) {
+        res.status(400).json()
+    }
+}
+
+const login = async (req,res) => {
+    try {
+        const user = await User.findOne({
+            email: req.body.email
+        }).exec();
+
+        if(user && (await user.isValidPassword(req.body.password))){
+            const token = jwtServices.generateAccessToken(user);
+            res.json(token);
+        }else{
+            res.status(400).json({
+                error: "Email or password incorrect"
+            })
+        }
+    } catch (error) {
+        res.status(400).json(error)
+    }
+}
