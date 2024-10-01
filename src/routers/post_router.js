1e878bea6caf41bd9945aab935e63b781ff88bf7
João Neto <j.altevirneto@gmail.com>
2024-10-01 11:09:15 -0300

    feat: add routes for post

---
+++ src/routers/post_router.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import post_controller from "../controllers/post_controller.js"
+
+const router = Router();
+
+
+router.post("/", post_controller.store)
+router.get("/", post_controller.index)
+router.get("/:id",post_controller.show ) 
+router.put("/:id", post_controller.update) 
+router.delete("/:id", post_controller.destroy)
+
+export default router
