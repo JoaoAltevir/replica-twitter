4084ff0ca6f2f4b209036f0ca25be7b87d43ae86
João Neto <j.altevirneto@gmail.com>
2024-10-01 11:07:10 -0300

    feat: add routes on main archive

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import "dotenv/config"
+import e from "express"
+import userRouter from "./routers/user_router.js"
+import postRouter from "./routers/post_router.js"
+const app = e()
+
+
+app.use(e.json);
+
+app.use("/user", userRouter);
+app.user("post", postRouter);
+
+app.listen(process.env.API_PORT, () => {
+    console.log(`listening on port ${process.env.API_PORT}`)
+})
