1db226436d9de5c138f8b933b8a202cdeceb5bfa
João Neto <j.altevirneto@gmail.com>
2024-10-01 11:08:43 -0300

    faet: add logics for verify and generate tokens

---
+++ src/services/jwt_services.js
@@ -0,0 +1,22 @@
+import jsonwebtoken from "jsonwebtoken";
+
+
+const generateAccessToken = (user) => {
+    const token = jsonwebtoken.sign({
+        _id: user._id,
+        email: user.email,
+    },
+    process.env.JWT_PRIVATE_KEY,
+    {
+        expiresIn: "1d"
+    })
+
+    return token
+}
+
+const verifyAccessToken = (token) => jsonwebtoken.verify(token, process.env.JWT_PRIVATE_KEY);
+
+export default {
+    generateAccessToken,
+    verifyAccessToken
+}
