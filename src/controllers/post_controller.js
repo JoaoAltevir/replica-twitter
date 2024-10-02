e65bf23bb91b6646bbb348f2e77206f5dc62a00d
João Neto <j.altevirneto@gmail.com>
2024-10-02 08:26:17 -0300

    feat: finished basic CRUD

--- src/controllers/post_controller.js
+++ src/controllers/post_controller.js
@@ -6,7 +6,7 @@ const store = async (req, res) => {
             text: req.body.text,
             user: req.user
         })
-        res.status(201).json()
+        res.status(201).json(user)
     } catch (error) {
         res.status(400).json()
     }
@@ -20,4 +20,39 @@ const index = async (req,res) => {
         console.log(error)
         res.status(400);json()
     }
+}
+
+const show = async (req, res) => {
+    try {
+        const content = await Post.findById(req.params.id).exec();
+        res.json(content)
+    } catch (error) {
+        console.log(error)
+        res.status(400).json();
+    }
+}
+
+const update = async(req, res) => {
+    try {
+        await Post.findByIdAndUpdate(req.params.id, req.body).exec()
+    } catch (error) {
+        console.log(error)
+        res.status(400).json();       
+    }
+}
+
+const destroy = async (req, res) => {
+    try {
+        await PostfindByIdAndDelete(req.params.id).exec();
+    } catch (error) {
+        res.status(401).json();
+    }
+}
+
+export default {
+    store,
+    index,
+    show,
+    update,
+    destroy
 }
