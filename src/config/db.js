b95ea43ca400da1916adcc7446ee58f12c516f62
João Altevir <j.altevirneto@gmail.com>
2024-10-02 23:00:51 -0300

    refact: trying connect in DB (not successfull)

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,15 @@
+import "dotenv/config"
 import mongoose from "mongoose";
 
-(async () => {
+const main = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB + process.env.DATABASE);
+    await mongoose.connect(`${process.env.MONGODB} + ${process.env.DATABASE}`);
     console.log("connected with DB");
   } catch (error) {
     console.log(error);
   }
-})();
+}
+
+main()
+
+export default mongoose
