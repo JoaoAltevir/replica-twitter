1ac30ff027c58bdd8b67ebcac059547905975f6f
João Neto <j.altevirneto@gmail.com>
2024-10-04 11:31:41 -0300

    reafact: rotes are all private

--- src/routers/post_router.js
+++ src/routers/post_router.js
@@ -3,9 +3,13 @@ import post_controller from "../controllers/post_controller.js"
 import jwtAuth from "../middlewares/jwt_auth.js"
 const router = Router();
 
+
+// ROTAS PUBLICAS
+
+//ROTAS PRIVADAS
+router.use(jwtAuth)
 router.get("/", post_controller.index)
 router.get("/:id",post_controller.show ) 
-router.use(jwtAuth)
 router.post("/", post_controller.store)
 router.put("/:id", post_controller.update) 
 router.delete("/:id", post_controller.destroy)
