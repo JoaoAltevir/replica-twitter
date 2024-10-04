903d421522272203cab2a969d08f6cac18872239
João Neto <j.altevirneto@gmail.com>
2024-10-04 10:33:26 -0300

    feat: init crud logic user

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -1,4 +1,29 @@
 import User from "../models/user_model.js";
+import jwtServices from "../services/jwt_services.js";
+
+
+const index = async (req, res) => {
+    try {
+        const content = await User.find(req.query).exec()
+        res.json(content);
+    } catch (error) {
+        res.status(400).json();
+    }
+}
+
+const destroy = async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id).exec();
+        res.json();
+    } catch (error) {
+        console.log(error);
+        res.status(403).json();
+    }
+}
+    
+
+
+///////////////
 
 
 const signup = async (req,res) => {
@@ -6,18 +31,18 @@ const signup = async (req,res) => {
         const user = await User.create({
             email: req.body.email,
             password: req.body.password,
-            nickname: req.body.nickname,
-        })
-
+            nickname: req.body.nickname
+        });
         const token = jwtServices.generateAccessToken(user)
 
         res.status(201).json(token)
     } catch (err) {
+        console.log(err)
         res.status(400).json()
     }
 }
 
-const login = async (req,res) => {c
+const login = async (req,res) => {
     try {
         const user = await User.findOne({
             email: req.body.email
@@ -38,5 +63,7 @@ const login = async (req,res) => {c
 
 export default {
     login,
-    signup
+    signup,
+    index,
+    destroy
 }
