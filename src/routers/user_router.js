fb1c2f2ae542c111ccc81740ae9bb50bfac1575f
João Neto <j.altevirneto@gmail.com>
2024-10-04 10:31:58 -0300

    feat: add project of authorizer

--- src/routers/user_router.js
+++ src/routers/user_router.js
@@ -1,10 +1,16 @@
 import {Router} from "express";
 import userController from "../controllers/user_controller.js"
+import authenticator from "../middlewares/jwt_auth.js"
+import authorizer from "../middlewares/authorizer.js";
 
 const router = Router()
 
 router.post("/signup", userController.signup);
 router.post("/login", userController.login);
 
+router.use(authenticator);
+router.use(authorizer);
+router.get("/", userController.index);
+router.delete("/:id", userController.destroy);
 
 export default router
